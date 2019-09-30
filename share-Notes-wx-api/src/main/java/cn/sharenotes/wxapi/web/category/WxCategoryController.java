dba2834f8d7394c610d233f0628f05cf180b5b6e
ZH46 <769050668@qq.com>
2019-09-30 17:13:53 +0800

    修改获取目录

--- share-Notes-wx-api/src/main/java/cn/sharenotes/wxapi/web/category/WxCategoryController.java
+++ share-Notes-wx-api/src/main/java/cn/sharenotes/wxapi/web/category/WxCategoryController.java
@@ -1,8 +1,11 @@
 package cn.sharenotes.wxapi.web.category;
 
+import cn.sharenotes.core.utils.ResponseUtil;
 import cn.sharenotes.db.model.dto.CategoryDTO;
 import cn.sharenotes.db.service.CategoriesService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * @author 76905
+ */
 @RestController
 @RequestMapping("/wx/category")
 public class WxCategoryController {
@@ -17,8 +23,13 @@ public class WxCategoryController {
     @Autowired
     CategoriesService categoriesService;
 
-    @PostMapping("/getAll/{userId}")
-    public List<CategoryDTO> getAllCategories(@PathVariable("userId") Integer userId){
-        return categoriesService.findCategoriesByUserOpenId(userId);
+    @ApiOperation(value = "通过 meanId 获取目录")
+    @PostMapping("/getAll/{userId}/{menuId}")
+    public Object getAllCategories(@PathVariable("userId") Integer userId,@PathVariable("menuId") Integer menuId){
+        List<CategoryDTO> categoryDTOS = categoriesService.findCategoriesByUserOpenId(userId,menuId);
+        if(CollectionUtils.isEmpty(categoryDTOS)){
+            return ResponseUtil.fail(601,"没有目录");
+        }
+        return ResponseUtil.ok(categoryDTOS);
     }
 }
