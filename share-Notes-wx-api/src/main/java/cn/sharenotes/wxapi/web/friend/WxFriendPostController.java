054717a576693c6ecba05b3adae1fb78da23c5c8
hu <3080585233@qq.com>
2019-10-08 11:28:37 +0800

    朋友目录下的内容

---
+++ share-Notes-wx-api/src/main/java/cn/sharenotes/wxapi/web/friend/WxFriendPostController.java
@@ -0,0 +1,88 @@
+package cn.sharenotes.wxapi.web.friend;
+
+import cn.sharenotes.core.service.PostContentService;
+import cn.sharenotes.core.utils.ResponseUtil;
+import cn.sharenotes.db.model.dto.PostDTO;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Post controller.
+ *
+ * @author kiwi
+ * @date 3/19/19
+ */
+@RestController
+@RequestMapping("/wx/friend")
+public class WxFriendPostController {
+
+
+    @Resource
+    private PostContentService postContentService;
+
+    @ApiOperation("Lists posts")
+    @GetMapping("/getPost/{cate_id}")
+    public Object getPosts(/*@LoginUser Integer userId,*/ @PathVariable("cate_id") Integer cate_id) {
+        List<PostDTO> postDTOS = postContentService.findPostsByUserId(3, cate_id);
+        Map<String, Object> result = new HashMap<>();
+        if (CollectionUtils.isEmpty(postDTOS)) {
+            return ResponseUtil.fail(711,"您朋友尚未创建文章");
+        } else {
+            result.put("posts", postDTOS);
+            return ResponseUtil.ok(result);
+        }
+    }
+
+
+
+
+
+// todo
+//    @GetMapping("{postId:\\d+}")
+//    public PostVO getBy(@PathVariable("postId") Integer postId) {
+//            获取文章
+//        return 文章;
+//    }
+
+
+
+
+
+    //      todo
+//    @PostMapping
+//    public PostVO createBy(@Valid @RequestBody  PostDto post,
+//                                 @RequestParam(value = "autoSave", required = false, defaultValue = "false") Boolean autoSave) {
+//        // TODO: 2019/9/28 创建文章，需要自动存储
+//    }
+
+
+
+
+
+// todo
+//    @PutMapping("{postId:\\d+}")
+//    public PostVO updateBy(@Valid @RequestBody PostDto postdto,
+//                                 @PathVariable("postId") Integer postId,
+//                                 @RequestParam(value = "autoSave", required = false, defaultValue = "false") Boolean autoSave) {
+//         获取文章id
+//        Post postToUpdate = postService.getById(postId);
+//        postdto.update(postToUpdate);
+//        自动更新
+//        return 更新进数据库;
+//    }
+
+
+
+    @DeleteMapping("{postId:\\d+}")
+    public void deletePermanently(@PathVariable("postId") Integer postId) {
+//       todo 删除文章
+    }
+
+
+}
