c132e0378df2ae96adc9a1ad09f6394c8a3b3889
ZH46 <769050668@qq.com>
2019-10-17 15:41:11 +0800

    获取三天的日志

--- share-Notes-wx-api/src/main/java/cn/sharenotes/wxapi/web/log/LogsController.java
+++ share-Notes-wx-api/src/main/java/cn/sharenotes/wxapi/web/log/LogsController.java
@@ -10,10 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * @author 76905
@@ -28,12 +26,27 @@ public class LogsController {
     @GetMapping("/getAll")
     public Object getAllLogs(@LoginUser Integer userId){
         Map<String,Object> result = new HashMap<>();
+        List<LogsDTO> today = new ArrayList<>();
+        List<LogsDTO> yesterday = new ArrayList<>();
+        List<LogsDTO> tdbYesterday = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
         List<LogsDTO> logsDTOS = logsService.getAllLogsByUserId(userId);
         if(CollectionUtils.isEmpty(logsDTOS)){
             return ResponseUtil.fail(501,"没有操作日志");
         }
-        Calendar c = Calendar.getInstance();
-        result.put("logs",logsDTOS);
+        for(LogsDTO logsDTO : logsDTOS){
+            if(sdf.format(date).equals(sdf.format(logsDTO.getCreateTime()))) {
+                today.add(logsDTO);
+            }else if(sdf.format(new Date(date.getTime()- 24*3600*1000L)).equals(sdf.format(logsDTO.getCreateTime()))){
+                yesterday.add(logsDTO);
+            }else if(sdf.format(new Date(date.getTime()- 2*24*3600*1000L)).equals(sdf.format(logsDTO.getCreateTime()))){
+                tdbYesterday.add(logsDTO);
+            }
+        }
+        result.put("today",today);
+        result.put("yesterday",yesterday);
+        result.put("tdbYesterday",tdbYesterday);
         return ResponseUtil.ok(result);
     }
 
