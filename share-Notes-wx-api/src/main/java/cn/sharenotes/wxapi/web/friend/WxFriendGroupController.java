068c34836c4eabc3f11c44c75bee59c0a568f4bc
hu <3080585233@qq.com>
2019-10-21 15:59:54 +0800

    朋友

--- share-Notes-wx-api/src/main/java/cn/sharenotes/wxapi/web/friend/WxFriendGroupController.java
+++ share-Notes-wx-api/src/main/java/cn/sharenotes/wxapi/web/friend/WxFriendGroupController.java
@@ -62,10 +62,11 @@ public class WxFriendGroupController {
         if(userId == null){
             return ResponseUtil.fail(709,"用户没有登陆");
         }
-        boolean b = userGroupsSerive.addFriend(userId, friendId);
+
         if(userId.equals(friendId)){
             return ResponseUtil.fail(709,"无需添加自己为好友");
         }
+        boolean b = userGroupsSerive.addFriend(userId, friendId);
         if(b){
             return ResponseUtil.ok();
         }
